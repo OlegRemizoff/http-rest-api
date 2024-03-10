028ea8dfa5d493ce825f7fcdf720bb6772b73fd8
Oleg Remizov <job.rem1zoff@gmail.com>
2024-03-10 14:14:27 +0300

    encrypted password method

--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -1,8 +1,32 @@
 package model
 
+import "golang.org/x/crypto/bcrypt"
+
 // User ...
 type User struct {
 	ID                 int
 	Email              string
+	Password           string
 	Encrypted_password string
+}
+
+// BeforeCreate
+func (u *User) BeforeCreate() error {
+	if len(u.Password) > 0 {
+		enc, err := encryptString(u.Password)
+		if err != nil {
+			return err
+		}
+		u.Encrypted_password = enc
+	}
+	return nil
+}
+
+func encryptString(s string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
 }
--- internal/app/store/userrepository_test.go
+++ internal/app/store/userrepository_test.go
@@ -12,9 +12,7 @@ func TestUserRepository_Create(t *testing.T) {
 	s, teardown := store.TestStore(t, "host=localhost dbname=restapi_test user=postgres password=postgres sslmode=disable")
 	defer teardown("users")
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@example.org",
-	})
+	u, err := s.User().Create(model.TestUser(t))
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
@@ -27,11 +25,11 @@ func TestUserRepository_FindByEmail(t *testing.T) {
 	_, err := s.User().FindByEmail(email)
 	assert.Error(t, err)
 
-	s.User().Create(&model.User{
-		Email: "user@example.org",
-	})
+	u := model.TestUser(t)
+	u.Email = email
+	s.User().Create(u)
 
-	u, err := s.User().FindByEmail(email)
+	u, err = s.User().FindByEmail(email)
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
